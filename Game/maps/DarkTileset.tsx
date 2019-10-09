4e2d82f42c936734b5ceee1e64e2a1cded83ed7e
OuEsquerra <paupe200@gmail.com>
2019-10-09 13:31:57 +0200

    tsx+tmx new Tiled 1.1.5

---
+++ Game/maps/DarkTileset.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="DarkTileset" tilewidth="32" tileheight="32" tilecount="666" columns="18">
+ <image source="DarkTileset.png" width="576" height="1184"/>
+ <tile id="197">
+  <objectgroup draworder="index">
+   <object id="12" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+</tileset>
