5d635737e6ee03a599c1a1fba88a4f6f47eb82c4
OuEsquerra <paupe200@gmail.com>
2019-10-09 17:21:13 +0200

    Player Module Filled

--- Motor2D/j1Player.cpp
+++ Motor2D/j1Player.cpp
@@ -1,2 +1,50 @@
 #include "j1App.h"
 #include "j1Player.h"
+#include "j1Module.h"
+#include "p2Point.h"
+#include "j1Render.h"
+
+
+	bool j1Player::Init() {
+	
+		return true;
+	};
+
+	bool j1Player::Awake(pugi::xml_node config) {
+
+
+		return true;
+	}; 
+
+	bool j1Player::Start() {
+
+		positionP1 = { 100,100 };
+		playerBox = { positionP1.x,positionP1.y,100,100 };
+
+
+		return true;
+	};
+
+	bool j1Player::preUpdate() {
+
+
+
+		return true;
+	};
+
+	bool j1Player::Update() {
+	
+		App->render->DrawQuad(playerBox,255,0,0,100);
+		return true;
+	};
+
+	bool j1Player::postUpdate() {
+
+		return true;
+	};
+
+	bool j1Player::cleanUp() {
+
+		return true;
+	};
+
--- Motor2D/j1Player.h
+++ Motor2D/j1Player.h
@@ -2,17 +2,40 @@
 #define __j1PLAYER_H__
 
 #include "j1Module.h"
+#include "p2Point.h"
+#include "j1Render.h"
 
 class j1Player : public j1Module {
-	//Methods
-public:
+public://methods
+	bool Init();
+
+	bool Awake(pugi::xml_node config);
+
+	bool Start();
+
+	bool preUpdate();
+
+	bool Update();
+
+	bool postUpdate();
+
+	bool cleanUp();
 
 private:
 
-	//Variables
-public:
+	
+public://variables
+
+	
+
+	
 
 private:
+
+	SDL_Rect playerBox;
+	iPoint positionP1;
+	
+	
 
 };
 
